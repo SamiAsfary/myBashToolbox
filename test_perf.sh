6b160eb56ccfcdc47d2aeaec2065f151cde451ed
Sami Asfary <s.asfary@gmail.com>
2025-08-21 00:18:00 +0200

    in test_perf, the option -t can be used to fetch the info from top instead of /proc

--- test_perf.sh
+++ test_perf.sh
@@ -1,48 +1,109 @@
 CPUn=(cpu cpu0 cpu1 cpu2 cpu3)
 
+outputFile=$1
+useTop=false
+delay=30
+
+oneLineNoTop () {
+    #Check time
+    echo -n `date +"%D;%T;"` >> $outputFile
+
+    #Check each CPU
+    for i in ${!CPUn[@]}
+    do 
+        resCPU=(`cat /proc/stat | grep "${CPUn[$i]} "`)
+        for j in $(seq 1 10); do
+            echo -n "${resCPU[$j]};" >> $outputFile
+        done
+    done
+
+    #Check Memory 
+    #meminfo=`cat /proc/meminfo` 
+    cat /proc/meminfo | grep -Eo "[0-9]+" | while read line;do
+        echo -n "${line};" >> $outputFile
+    done
+}
+
+oneLineTop () {
+    #Check time
+    echo -n `date +"%D;%T;"` >> $outputFile
+
+    #Check CPU top  us;sy;ni;id;wa;hi;si;st
+    resCPUtop=(`top -n1 -b | head -n 3 | tail -n 1`)
+    for i in $(seq 1 2 16);do
+        echo -n "${resCPUtop[i]};" >> $outputFile
+    done
+
+    #Check Memory top
+    resMemTop=(`top -n1 -b | head -n 4 | tail -n 1`)
+    for i in $(seq 3 2 9);do
+        echo -n "${resMemTop[i]};" >> $outputFile
+    done
+    #echo -n "${resMem[3]};${resMem[5]};${resMem[7]};${resMem[9]};" >> $outputFile
+}
+
+LoopNoTop () {
+    while true; do
+        oneLineNoTop
+        echo "" >> $outputFile
+        sleep $delay
+    done
+}
+
+LoopTop () {
+    while true; do
+        oneLineTop
+        echo "" >> $outputFile
+        sleep $delay
+    done
+}
+
+
+#check if using arguments or not
+if [[ $# -gt 1 ]]; then
+    for args in "$@"; do
+        case $args in
+            -t)
+                useTop=true
+                ;;
+            *)
+                ;;
+        esac
+    done
+fi
+
+if $useTop; then
+    LoopTop
+fi
+LoopNoTop
+
 #Time;CPUusr;CPUnice;CPUsys;CPUidle;CPUsoftIRQ;CPUiusr;CPUinice;CPUisys;CPUiidle;CPUisoftIRQ;
 while true; do
 
 #allJiffies
 # sudo cat /proc/timer_list | grep 'jiffies:' | grep -Eo "[0-9]+" | while read line;do
-#     echo -n "${line};" >> $1
+#     echo -n "${line};" >> $outputFile
 # done
 
-#Check time
-echo -n `date +"%D;%T;"` >> $1
 
-#Check each CPU
-for i in ${!CPUn[@]}
-do 
-    resCPU=(`cat /proc/stat | grep "${CPUn[$i]} "`)
-    for j in $(seq 1 10); do
-        echo -n "${resCPU[$j]};" >> $1
-    done
-done
-
-#Check Memory 
-#meminfo=`cat /proc/meminfo` 
-cat /proc/meminfo | grep -Eo "[0-9]+" | while read line;do
-    echo -n "${line};" >> $1
-done
 
 #Check Memory top
 # resMem=(`top -n1 -b | head -n4 | tail -n1`)
-# echo -n "${resMem[3]};${resMem[5]};${resMem[7]};${resMem[9]};" >> $1
+# echo -n "${resMem[3]};${resMem[5]};${resMem[7]};${resMem[9]};" >> $outputFile
 
 # #Check CPU top  us;sy;ni;id;wa;hi;si;st
 # resCPUtop=(`top -n1 -b | head -n3 | tail -n1`)
 # for i in $(seq 1 2 16);do
-# echo -n "${resCPUtop[i]};" >> $1
+# echo -n "${resCPUtop[i]};" >> $outputFile
 # done
 
 #debugDeltaTime
-#echo -n `date +"%D;%T;"` >> $1
+#echo -n `date +"%D;%T;"` >> $outputFile
 # sudo cat /proc/timer_list | grep 'jiffies:' | grep -Eo "[0-9]+" | while read line;do
-#     echo -n "${line};" >> $1
+#     echo -n "${line};" >> $outputFile
 # done
 #LineReturn
-echo "" >> $1
+echo "" >> $outputFile
 sleep 30
 done
 #cat /proc/stat | grep "^cpu "
