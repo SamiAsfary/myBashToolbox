329a0d2b3bec910ab6141b3df9c32fbdd51209c1
Sami Asfary <s.asfary@gmail.com>
2025-11-07 22:41:55 +0100

    in extractLogs, added compression and cleaned up boot selection

--- extractLogs.sh
+++ extractLogs.sh
@@ -4,6 +4,10 @@
 
 srcOption="-t"
 srcField="SYSLOG_IDENTIFIER"
+needCompressing=false
+tarCompressing=false
+zipCompressing=false
+tgzCompressing=false
 
 if [[ $# -gt 1 ]]; then
     for args in "$@"; do
@@ -12,21 +16,46 @@ if [[ $# -gt 1 ]]; then
                 srcOption="-u"
                 srcField="_SYSTEMD_UNIT"
                 ;;
+            -ct)
+                needCompressing=true
+                tarCompressing=true
+                ;;
+            -cz)
+                needCompressing=true
+                zipCompressing=true
+                ;;
+            -ctg)
+                needCompressing=true
+                tgzCompressing=true
+                ;;
             *)
                 ;;
         esac
     done
 fi
 
-extractBoot=0
 extractName=${!#}
+extractTime="-b 0"
 
 journalctl --list-boot > ${extractName}_extractInfo
 
 journalctl --field=$srcField | while read iden; do
-    if [[ $(journalctl -n 1 -b -${extractBoot} $srcOption ${iden}) != "-- No entries --" ]]; then
-        journalctl -b -${extractBoot} $srcOption ${iden} > "${extractName}_${iden}"
+    if [[ $(journalctl -n 1 ${extractTime} $srcOption ${iden}) != "-- No entries --" ]]; then
+        journalctl ${extractTime} $srcOption ${iden} > "${extractName}_${iden}"
     else
-        echo "[${iden}] does not have any entries for boot -${extractBoot}" >> ${extractName}_extractInfo
+        echo "[${iden}] does not have any entries for options ${extractTime}" >> ${extractName}_extractInfo
     fi
 done
+
+if $needCompressing;then
+    if $tarCompressing;then
+        tar -cvf archive_${extractName}.tar ${extractName}_*
+    elif $zipCompressing;then
+        zip archive_${extractName}.zip ${extractName}_*
+    elif $tgzCompressing;then
+        tar -czvf archive_${extractName}.tgz ${extractName}_*
+    else
+        exit 1
+    fi
+    rm ${extractName}_*
+fi
